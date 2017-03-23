60d7fa856cdb08355da341f42f0dbbb03a250a5e
Joonas Kurki <joonas.kurki@helsinki.fi>
2017-03-23 23:36:52 +0200

    testien luomista

--- Roolipeli/src/test/java/juuri/sovelluslogiikka/hahmo/HahmoTest.java
+++ Roolipeli/src/test/java/juuri/sovelluslogiikka/hahmo/HahmoTest.java
@@ -1,6 +1,7 @@
 
 package juuri.sovelluslogiikka.hahmo;
 
+import juuri.apuvalineet.Sijainti;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -9,8 +10,43 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HahmoTest {
+    private Hahmo hahmo;
+    
     @Before
     public void setUp() {
+    }
+    
+    @Test
+    public void druidiLuodaanOikein() {
+        hahmo = new Druidi("Jakke");
+        assertEquals("Jakke", hahmo.getNimi());
+        
+        Sijainti ss = new Sijainti();
+        ss.setSijainti(1, 1);
+        assertEquals(ss, hahmo.getSijainti());
+        assertEquals("druidi", hahmo.getAmmatti());
+    }
+    
+    @Test
+    public void velhoLuodaanOikein() {
+        hahmo = new Velho("Anne");
+        assertEquals("Anne", hahmo.getNimi());
+        
+        Sijainti ss = new Sijainti();
+        ss.setSijainti(1, 1);
+        assertEquals(ss, hahmo.getSijainti());
+        assertEquals("velho", hahmo.getAmmatti());
+    }
+    
+    @Test
+    public void soturiLuodaanOikein() {
+        hahmo = new Soturi("Tosimies");
+        assertEquals("Tosimies", hahmo.getNimi());
+        
+        Sijainti ss = new Sijainti();
+        ss.setSijainti(1, 1);
+        assertEquals(ss, hahmo.getSijainti());
+        assertEquals("soturi", hahmo.getAmmatti());
     }
     
     
